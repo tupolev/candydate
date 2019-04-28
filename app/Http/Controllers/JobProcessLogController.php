ca771c729aabc4dcab7ab12c507b85f2f8bcd0b8
tupolev <tupolev@gmail.com>
2019-04-28 02:19:56 +0200

    Completed edit job process and created job process log controller

--- app/Http/Controllers/JobProcessLogController.php
+++ app/Http/Controllers/JobProcessLogController.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Exceptions\JobProcess\JobProcessCreateException;
-use App\JobProcess;
 use App\JobProcessLog;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
