dbc55fca843a5c866605d974db002d2a6ca80d98
tupolev <tupolev@gmail.com>
2019-05-01 18:14:01 +0200

    add oauth keys setup script to composer

--- heroku/setup_oauth_keys.sh
+++ heroku/setup_oauth_keys.sh
@@ -6,7 +6,7 @@ curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && python get-pip.py --p
 
 echo "Downloading and installing awscli"
 
-$HOME/.local/bin/pip install awscli --prefix=.local --yes
+.local/bin/pip install awscli --prefix=.local --yes
 
 echo "Fetching oauth private key from cloudcube"
 AWS_ACCESS_KEY_ID=$CLOUDCUBE_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$CLOUDCUBE_SECRET_ACCESS_KEY .local/bin/aws s3 cp s3://cloud-cube-eu/zrbqx2c35o3p/oauth-private.key storage/oauth-private.key
