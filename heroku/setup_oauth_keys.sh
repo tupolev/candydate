f3746015809c4ca8db7039f32b61626c1f179dcb
tupolev <tupolev@gmail.com>
2019-05-01 18:36:38 +0200

    add oauth keys setup script to composer

--- heroku/setup_oauth_keys.sh
+++ heroku/setup_oauth_keys.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-mkdir /bin
-export VENDORDIR=$HOME
-
 echo "Downloading and installing pip"
 
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && python get-pip.py --prefix=$VENDORDIR/.local
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && python get-pip.py --prefix=$HOME/.local
 
 echo "Downloading and installing awscli"
 
-$VENDORDIR/.local/bin/pip install awscli --prefix=$VENDORDIR/.local --yes
+$HOME/.local/bin/pip install awscli --prefix=$HOME/.local --yes
 
 echo "Fetching oauth private key from cloudcube"
-AWS_ACCESS_KEY_ID=$CLOUDCUBE_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$CLOUDCUBE_SECRET_ACCESS_KEY $VENDORDIR/.local/bin/aws s3 cp s3://cloud-cube-eu/zrbqx2c35o3p/oauth-private.key /storage/oauth-private.key
+AWS_ACCESS_KEY_ID=$CLOUDCUBE_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$CLOUDCUBE_SECRET_ACCESS_KEY $HOME/.local/bin/aws s3 cp s3://cloud-cube-eu/zrbqx2c35o3p/oauth-private.key $HOME/storage/oauth-private.key
 
 echo "Fetching oauth public key from cloudcube"
-AWS_ACCESS_KEY_ID=$CLOUDCUBE_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$CLOUDCUBE_SECRET_ACCESS_KEY $VENDORDIR/.local/bin/aws s3 cp s3://cloud-cube-eu/zrbqx2c35o3p/oauth-public.key /storage/oauth-public.key
+AWS_ACCESS_KEY_ID=$CLOUDCUBE_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$CLOUDCUBE_SECRET_ACCESS_KEY $HOME/.local/bin/aws s3 cp s3://cloud-cube-eu/zrbqx2c35o3p/oauth-public.key $HOME/storage/oauth-public.key
 
 echo "Finished oauth keys setup"
 
